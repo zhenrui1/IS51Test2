3ef76807c0f246785eefed77e915876225d5b7c1
zhenrui1 <zhenrui1219@mail.fresnostate.edu>
2022-05-06 17:30:06 -0700

    finish structured english

---
+++ istest_2.py
@@ -0,0 +1,7 @@
+
+"""
+The program is trying to display student grades on a final exam.
+A text file called "Final.txt" contains 24 students' grades
+It is required to calculate number of grades, the average grade and the grades that are above the average
+Then print out output, "Number of grades", "Average grade", "Percentage of grades above average".
+"""
